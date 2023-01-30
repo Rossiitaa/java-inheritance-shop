0312a8ebae421566e5dd935971365f3a8643bf4e
Rossiitaa <rosscuotto1998@gmail.com>
2023-01-30 16:26:40 +0100

    Cuffie class added

---
+++ java-inheritance-shop/src/org/lessons/java/shop/Cuffie.java
@@ -0,0 +1,33 @@
+package org.lessons.java.shop;
+
+public class Cuffie extends Prodotto {
+	private String colore;
+	private boolean wireless;
+
+	Cuffie(int codice, String nome, String marca, double prezzo, int iva, String colore, boolean wireless) {
+		super(codice, nome, marca, prezzo, iva);
+		this.colore = colore;
+		this.wireless = wireless;
+	}
+
+	public String getColore() {
+		return colore;
+	}
+
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+
+	public boolean isWireless() {
+		return wireless;
+	}
+
+	public void setWireless(boolean wireless) {
+		this.wireless = wireless;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\ncolore: " + getColore() + "\nwireless: " + isWireless();
+	}
+}
